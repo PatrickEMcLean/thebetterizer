d15b0edaa18dd31d0face11cef03881cc3135d32
Scott Helm <helm.scott@gmail.com>
2013-08-25 10:37:41 -0400

    creates seeds for role and scott admin user - patrick to add his user info here.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+role_admin = Role.find_or_create_by_key_and_label(key: "admin", label: "Admin")
+role_coach = Role.find_or_create_by_key_and_label(key: "coach", label: "Coach")
+
+scott_admin = User.where(
+  provider: "google_oauth2",
+  uid: "104542683808519361711").first
+
+unless scott_admin
+  scott_admin = User.create(
+    provider: "google_oath2",
+    uid: "104542683808519361711",
+    name: "Scott Helm",
+    email: "helm.scott@gmail.com",
+    image: "https://lh4.googleusercontent.com/-htHglKVHpO0/AAAAAAAAAAI/AAAAAAAAAWU/pkq-oyAzbN4/photo.jpg",
+    created_at: Time.now,
+    updated_at: Time.now)
+end
+
+scott_admin.roles << role_admin
